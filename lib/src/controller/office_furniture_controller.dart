f9a3c9dcd574cd81fb3b79530493fc1386c0994d
sinasystem <sinasys>
2022-06-09 13:35:48 +0430

    Add clear button for cart screen

--- lib/src/controller/office_furniture_controller.dart
+++ lib/src/controller/office_furniture_controller.dart
@@ -58,4 +58,9 @@ class OfficeFurnitureController extends GetxController {
     }
   }
 
+  void clearCart(){
+    cartFurniture.clear();
+   update();
+  }
+
 }
--- lib/src/view/screen/cart_screen.dart
+++ lib/src/view/screen/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:office_furniture_store/core/app_color.dart';
 import 'package:office_furniture_store/core/app_style.dart';
 import 'package:office_furniture_store/src/controller/office_furniture_controller.dart';
 import 'package:office_furniture_store/src/view/widget/empty_widget.dart';
@@ -10,12 +11,26 @@ import 'home_screen.dart';
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
 
+  PreferredSizeWidget _appBar(){
+    return AppBar(
+      title: const Text("Cart", style: h2Style),
+      actions: [
+        IconButton(
+          splashRadius: 20.0,
+          onPressed: controller.clearCart,
+          icon: const Icon(
+            Icons.delete,
+            color: AppColor.lightBlack,
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Cart",style: h2Style),
-      ),
+      appBar: _appBar(),
       bottomNavigationBar: Obx(
         () {
           return BottomBar(
@@ -26,15 +41,16 @@ class CartScreen extends StatelessWidget {
           );
         },
       ),
-      body:GetBuilder(
-        builder: (OfficeFurnitureController controller){
-          return controller.cartFurniture.isNotEmpty?
-          const Padding(
-            padding: EdgeInsets.all(15),
-            child: CartListView(),
-          ): const EmptyWidget(title: "Empty");
+      body: GetBuilder(
+        builder: (OfficeFurnitureController controller) {
+          return controller.cartFurniture.isNotEmpty
+              ? const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: CartListView(),
+                )
+              : const EmptyWidget(title: "Empty");
         },
-      )
+      ),
     );
   }
 }
