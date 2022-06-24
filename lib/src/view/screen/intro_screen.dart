d74a9492638aa414869a182656b57e0769ac29bc
sinasystem <sinasys>
2022-06-24 12:29:41 +0430

    Solve intro button overflow problem

--- lib/src/view/screen/intro_screen.dart
+++ lib/src/view/screen/intro_screen.dart
@@ -7,41 +7,38 @@ class IntroScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    Widget introButton(){
-      return  Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 80),
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => const HomeScreen()),
-            );
-          },
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all<EdgeInsetsGeometry?>(
-              const EdgeInsets.all(8),
-            ),
-            backgroundColor: MaterialStateProperty.all(
-                Colors.white.withOpacity(0.4)),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25.0),
+    Widget introButton() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 60),
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: ElevatedButton.icon(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const HomeScreen()),
+              );
+            },
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all<EdgeInsetsGeometry?>(
+                const EdgeInsets.all(8),
+              ),
+              backgroundColor:
+              MaterialStateProperty.all(Colors.white.withOpacity(0.4)),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
               ),
             ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Spacer(),
-              Text("Get Started", style: h2Style.copyWith(color: Colors.white)),
-              const Spacer(),
-              CircleAvatar(
-                backgroundColor: Colors.white.withOpacity(0.2),
-                child: const Icon(Icons.arrow_forward,
-                    color: Colors.white),
-              )
-            ],
+            icon: CircleAvatar(
+              backgroundColor: Colors.white.withOpacity(0.2),
+              child: const Icon(Icons.arrow_back, color: Colors.white),
+            ),
+            label: Text(
+              "Get started",
+              style: h3Style.copyWith(color: Colors.white),
+            ),
           ),
         ),
       );
@@ -62,12 +59,16 @@ class IntroScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const Spacer(flex: 4),
-                Text("Kana Office\nStanding\nDesk", style: h1Style.copyWith(color: Colors.white)),
+                const Spacer(flex: 3),
+                Text(
+                  "Kana Office\nStanding\nDesk",
+                  style: h1Style.copyWith(color: Colors.white),
+                ),
                 const SizedBox(height: 5),
                 Text(
-                    "Create a sustainable work space with 100% natural bamboo",
-                    style: h2Style.copyWith(color: Colors.white)),
+                  "Create a sustainable work space with 100% natural bamboo",
+                  style: h3Style.copyWith(color: Colors.white),
+                ),
                 const SizedBox(height: 20),
                 introButton(),
                 const Spacer(),
