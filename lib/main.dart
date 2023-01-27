f4aca37d5f26fc260a561b94bb0dc3564813de15
sina <sinasystem47@gmail.com>
2023-01-28 00:28:17 +0330

    Add RepositoryProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' show PointerDeviceKind;
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:office_furniture_store/core/app_theme.dart';
+import 'package:office_furniture_store/src/business_logic/cubit/furniture/furniture_cubit.dart';
+import 'package:office_furniture_store/src/data/repository/repository.dart';
 import 'package:office_furniture_store/src/presentation/screen/intro_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -10,17 +13,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // Enable horizontal scroll on flutter web
-      scrollBehavior: const MaterialScrollBehavior().copyWith(
-        dragDevices: {
-          PointerDeviceKind.mouse,
-          PointerDeviceKind.touch,
-        },
+    return RepositoryProvider<Repository>(
+      create: (context) => Repository(),
+      child: BlocProvider<FurnitureCubit>(
+        create: (context) => FurnitureCubit(repository: context.read<Repository>()),
+        child: MaterialApp(
+          // Enable horizontal scroll on flutter web
+          scrollBehavior: const MaterialScrollBehavior().copyWith(
+            dragDevices: {
+              PointerDeviceKind.mouse,
+              PointerDeviceKind.touch,
+            },
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const IntroScreen(),
+          theme: AppTheme.lightTheme,
+        ),
       ),
-      home: const IntroScreen(),
-      theme: AppTheme.lightTheme,
     );
   }
 }
