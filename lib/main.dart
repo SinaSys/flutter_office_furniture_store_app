c31ab90b84166a48e26f869f071201c26cd9209c
sina <sinasystem47@gmail.com>
2023-01-28 00:17:53 +0330

    Cubit version : Enable horizontal scroll on flutter web

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:ui' show PointerDeviceKind;
 import 'package:office_furniture_store/core/app_theme.dart';
-import 'package:office_furniture_store/src/data/repository/repository.dart';
 import 'package:office_furniture_store/src/presentation/screen/intro_screen.dart';
-import 'package:office_furniture_store/src/business_logic/cubit/furniture/furniture_cubit.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,17 +10,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider<Repository>(
-      create: (context) => Repository(),
-      child: BlocProvider<FurnitureCubit>(
-        create: (context) =>
-            FurnitureCubit(repository: context.read<Repository>()),
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: const IntroScreen(),
-          theme: AppTheme.lightTheme,
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Enable horizontal scroll on flutter web
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+        },
       ),
+      home: const IntroScreen(),
+      theme: AppTheme.lightTheme,
     );
   }
 }
